656e846544c74ea0bb3774fae27441a1f9463518
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-01-18 10:21:50 +0530

    coded functionality for registration

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,18 +1,68 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.scss";
+import axios from "axios";
 
 const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", input);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  console.log(err);
+
   return (
     <div className="register">
       <div className="card">
         <div className="left">
           <h1>Register.</h1>
           <form>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Register</button>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <span style={{ color: "black" }}>{err && err}</span>
+            <button onClick={handleClick}>Register</button>
           </form>
         </div>
         <div className="right">
