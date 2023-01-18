1a81c9008c4be13944dec6ef2d2e8e462f613c78
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-01-18 10:23:14 +0530

    added and managed new middleware dependencies

--- server/index.js
+++ server/index.js
@@ -8,8 +8,17 @@ import commentRoutes from "./routes/comments.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+// Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
