89483f15b4889e8e3a77506f848e01b49da362ca
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-01-25 17:07:32 +0530

    Using multer for uploading files to the database

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import multer from "multer";
 
 // Middleware
 app.use((req, res, next) => {
@@ -20,6 +21,22 @@ app.use(
   })
 );
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/likes", likeRoutes);
