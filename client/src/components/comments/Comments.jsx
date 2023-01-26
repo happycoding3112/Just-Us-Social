9061842ae656395315f1e6ee8a35ca08ee4f2ce2
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-01-26 12:57:59 +0530

    Adding Comment component to avoid code redundancy

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,46 +1,60 @@
-import { useContext } from "react";
-import { AuthContext } from "../../context/authContext";
 import "./comments.scss";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import Comment from "../comment/Comment";
 
-const Comments = () => {
+const Comments = ({ postId }) => {
   const { currentUser } = useContext(AuthContext);
 
-  const comments = [
-    {
-      id: 1,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "John Doe",
-      userId: 1,
-      profilePicture:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  const [desc, setDesc] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery(["comments"], () =>
+    makeRequest.get("/comments?postId=" + postId).then((res) => {
+      return res.data;
+    })
+  );
+
+  const mutation = useMutation(
+    (newComment) => {
+      return makeRequest.post("/comments", newComment);
     },
     {
-      id: 2,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Jane Doe",
-      userId: 2,
-      profilePicture:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-  ];
+      onSuccess: () => {
+        queryClient.invalidateQueries(["comments"]);
+      },
+    }
+  );
+
+  const makePost = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc, postId });
+    setDesc("");
+  };
 
   return (
     <div className="comments">
       <div className="write">
         <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="Write a comment" />
-        <button>Post</button>
+        <input
+          type="text"
+          name="desc"
+          value={desc}
+          placeholder="Write a comment"
+          onChange={(e) => {
+            setDesc(e.target.value);
+          }}
+        />
+        <button onClick={makePost}>Post</button>
       </div>
-      {comments.map((comment) => (
-        <div className="comment" key={comment.id}>
-          <img src={comment.profilePicture} alt="" />
-          <div className="info">
-            <span>{comment.name}</span>
-            <p>{comment.desc}</p>
-          </div>
-          <div className="date">1 min ago</div>
-        </div>
-      ))}
+      {error
+        ? "Something went wrong!"
+        : isLoading
+        ? "Loading"
+        : data.map((comment) => <Comment key={comment.id} comment={comment} />)}
     </div>
   );
 };
