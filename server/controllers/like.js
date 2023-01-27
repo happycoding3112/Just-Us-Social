e58c28de507506965a054fed11543804e5beaa5b
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-01-27 19:03:24 +0530

    Wrote logic for fetching, adding and deleting User Likes

---
+++ server/controllers/like.js
@@ -0,0 +1,47 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
+export const getLikes = (req, res) => {
+  const q = "SELECT userId FROM likes WHERE postId = ?";
+
+  db.query(q, [req.query.postId], (err, data) => {
+    if (err) return res.status(500).json("Internal Server Error!");
+    return res.status(200).json(data.map((like) => like.userId));
+  });
+};
+
+export const addLike = (req, res) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json("User not logged in!");
+
+  jwt.verify(token, "secretKey", (err, userInfo) => {
+    if (err) return res.status(403).json("Request Denied");
+
+    const q = "INSERT INTO likes (`userId`, `postId`) VALUES (?)";
+
+    const values = [userInfo.id, req.body.postId];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json("Internal Server Error!");
+      return res.status(200).json("Liked the post successfully!");
+    });
+  });
+};
+
+export const deleteLike = (req, res) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json("User not logged in!");
+
+  jwt.verify(token, "secretKey", (err, userInfo) => {
+    if (err) return res.status(403).json("Request Denied");
+
+    const q = "DELETE FROM likes WHERE userId = (?) AND postId = (?)";
+
+    db.query(q, [userInfo.id, req.query.postId], (err, data) => {
+      if (err) return res.status(500).json("Internal Server Error!");
+      return res.status(200).json("Removed Like from the Post");
+    });
+  });
+};
