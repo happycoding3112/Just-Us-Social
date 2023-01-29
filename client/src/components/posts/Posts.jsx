35883a4b97100091aaab02466099ffc86fa7e1ed
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-01-30 00:49:17 +0530

    Wrote logic for conditional of Posts

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,15 +4,15 @@ import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
 const Posts = ({ userId }) => {
-  const { isLoading, error, data } = useQuery(["posts"], () => {
+  const { isLoading, error, data } = useQuery(["posts"], () =>
     userId
       ? makeRequest.get("/posts?userId=" + userId).then((res) => {
           return res.data;
         })
       : makeRequest.get("/posts").then((res) => {
           return res.data;
-        });
-  });
+        })
+  );
 
   return (
     <div className="posts">
