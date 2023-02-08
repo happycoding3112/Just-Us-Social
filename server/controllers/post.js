d99dd7c75c616dc21a93dcf38d712ed36aee3e41
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-02-08 12:04:07 +0530

    minor changes

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -11,11 +11,13 @@ export const getPosts = (req, res) => {
   jwt.verify(token, "secretKey", (err, userInfo) => {
     if (err) return res.status(403).json("Request Denied");
 
-    const q = userId
-      ? "SELECT p.*, u.id as userId, name, profilePic FROM posts as p JOIN users as u ON (u.id = p.userId) WHERE p.userId = (?) ORDER BY p.createdAt DESC"
-      : "SELECT p.*, u.id as userId, name, profilePic FROM posts as p JOIN users as u ON (u.id = p.userId) LEFT JOIN relationships as r ON (p.userId = r.followedUserId) WHERE r.followerUserId = (?) OR p.userId = (?) ORDER BY p.createdAt DESC";
+    const q =
+      userId !== "undefined"
+        ? "SELECT p.*, u.id as userId, name, profilePic FROM posts as p JOIN users as u ON (u.id = p.userId) WHERE p.userId = (?) ORDER BY p.createdAt DESC"
+        : "SELECT p.*, u.id as userId, name, profilePic FROM posts as p JOIN users as u ON (u.id = p.userId) LEFT JOIN relationships as r ON (p.userId = r.followedUserId) WHERE r.followerUserId = (?) OR p.userId = (?) ORDER BY p.createdAt DESC";
 
-    const values = userId ? [userId] : [userInfo.id, userInfo.id];
+    const values =
+      userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
 
     db.query(q, values, (err, data) => {
       if (err) return res.status(500).json(err);
